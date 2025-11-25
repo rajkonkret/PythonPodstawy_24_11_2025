a7f3456ecfc10909c27649d28e43aac36f680436
rajkonkre <rajkonkret660@gmail.com>
2025-11-25 13:55:46 +0100

    [2 21] update typy_danych_5_slownik.py

---
+++ day2/typy_danych_5_slownik.py
@@ -0,0 +1,36 @@
+# słownik - para klucz - wartość
+# {"user" :"Radek", "wiek":35}
+# odpowiednik jsona
+# klucze nie mogą się powtarzać
+
+# pusty słownik
+dictionary = {}
+print(dictionary)  # {}
+print(type(dictionary))  # <class 'dict'>
+
+dictionary_1 = dict()
+print(dictionary_1)  # {}
+print(type(dictionary_1))  # <class 'dict'>
+
+# dodanie elementu do słownika
+dictionary['imie'] = "Radek"
+print(dictionary)  # {'imie': 'Radek'}
+dictionary['wiek'] = 45
+print(dictionary)  # {'imie': 'Radek', 'wiek': 45}
+
+print(dictionary.keys())  # dict_keys(['imie', 'wiek'])
+print(dictionary.values())  # dict_values(['Radek', 45])
+print(dictionary.items())  # dict_items([('imie', 'Radek'), ('wiek', 45)])
+
+dictionary['imie'] = "Tomek"
+print(dictionary)  # {'imie': 'Tomek', 'wiek': 45}
+
+# wypisanie wartości dla klucza
+print(dictionary['imie'])  # Tomek
+
+dictionary['imie'] = ["Radek", "Tomek", "Magda"]
+print(dictionary)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 45}
+print(dictionary['imie'])  # ['Radek', 'Tomek', 'Magda']
+print(dictionary['imie'][0])  # Radek
+
+print(dictionary["Imie"])
