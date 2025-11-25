f3f8789ba8860b88f7008d3c8dca5863915083f4
rajkonkre <rajkonkret660@gmail.com>
2025-11-25 15:46:46 +0100

    [2 30] update warunki

--- day2/warunki.py
+++ day2/warunki.py
@@ -53,20 +53,31 @@ if (n := len(a)) > 3:
     print(f"Długośc wynosi {n}, więcej niż 3")
 # Długośc wynosi 5, więcej niż 3
 
-podatek = 0
-zarobki = int(input("Podaj zarobki:"))
-# kolejnośc ma znaczenia
-if zarobki < 10_000:
-    podatek = 0
-elif zarobki < 40_000:
-    # elif zarobki < 40_000:
-    podatek = 0.2
-elif zarobki < 100_000:
-    podatek = 0.4
-else:  # pozostałe
-    podatek = 0.9
+# podatek = 0
+# zarobki = int(input("Podaj zarobki:"))
+# # kolejnośc ma znaczenia
+# if zarobki < 10_000:
+#     podatek = 0
+# elif zarobki < 40_000:
+#     # elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:  # pozostałe
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# # podatek 0.2, od 10000 do 39999
+# # Podaj zarobki:15000
+# # Podatek wynosi: 3000.0 pln.
 
-print(f"Podatek wynosi: {zarobki * podatek} pln.")
-# podatek 0.2, od 10000 do 39999
-# Podaj zarobki:15000
-# Podatek wynosi: 3000.0 pln.
+suma_zam = 150
+if suma_zam > 100:
+    rabat = 25
+else:
+    rabat = 0
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
+
+# operator warumkowy
+rabat = 25 if suma_zam > 100 else 0
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
