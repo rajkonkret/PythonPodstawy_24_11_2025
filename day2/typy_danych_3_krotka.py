9767a1ebf41fec04b1758e270d8b952c70519cf3
rajkonkre <rajkonkret660@gmail.com>
2025-11-25 12:02:31 +0100

    [2 12] update typy_danych_3_krotka.py.py

--- day2/typy_danych_3_krotka.py
+++ day2/typy_danych_3_krotka.py
@@ -1,3 +1,37 @@
 # krotka - tuple - lista tylko do odczytu, niemutowalne
 # pozwala efektywniej zarządzać pamięcią
-# krotka jedoeleemntowej - stała, szczególny przypadek zmiennej
+# krotka jednoelementowej - stała, szczególny przypadek zmiennej
+
+tupla_imiona = "Zenek", "Marek", "Radek", "Ania"
+print(type(tupla_imiona))  # <class 'tuple'>
+print(tupla_imiona)  # ('Zenek', 'Marek', 'Radek', 'Ania')
+
+# tupla liczbowa
+# tupla_liczby = 43, 55, 22.34, 11, 200
+tupla_liczby = (43, 55, 22.34, 11, 200)
+print(type(tupla_liczby))  # <class 'tuple'>
+print(tupla_liczby)  # (43, 55, 22.34, 11, 200)
+
+# tupla jedoelementową
+tupla_jeden = 45,
+print(tupla_jeden)
+print(type(tupla_jeden))
+# (45,)
+# <class 'tuple'>
+
+# PEP8 zaleca by jednoelemntowe tuple robic z nawiasami
+tupla_jeden = (45,)
+print(tupla_jeden)
+print(type(tupla_jeden))
+
+# tupla jest niemutowalne
+# tupla_jeden[0] = 123  # TypeError: 'tuple' object does not support item assignment
+
+# usunięcie całej tupli
+del tupla_jeden
+# print(tupla_jeden)# NameError: name 'tupla_jeden' is not defined
+
+print(tupla_imiona.index("Radek"))  # index 2
+print(tupla_imiona.count("Radek"))  # wystepuje 1 raz
+
+print(len(tupla_imiona))  # długośc 4
