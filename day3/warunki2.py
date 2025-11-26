377235cc27c0b80f266a47fb952c1075261563d0
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 09:43:50 +0100

    [3 2] update warunki2

---
+++ day3/warunki2.py
@@ -0,0 +1,19 @@
+# od python 3.10
+# match case
+
+lista = []
+lang = input("Podaj znany Ci język programowania")
+
+match lang.strip().casefold():
+    case "python":
+        lista.append("Znam Pythona")
+    case 'java':
+        lista.append("Znam Jave")
+    case _:  # odpowiednik else
+        print('Nie znam takiego języka')
+
+print(lista)
+# Podaj znany Ci język programowaniajava
+# ['Znam Jave']
+#
+# Process finished with exit code 0
