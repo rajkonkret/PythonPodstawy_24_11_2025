4b8578d5f8b04c10e146f8fffcd56fd4a2a33f5e
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 10:38:09 +0100

    [3 6] update random_zad1.py

--- day3/random_zad1.py
+++ day3/random_zad1.py
@@ -17,3 +17,14 @@ lista = [67, 45, 32, 68, 90, 42, 69]
 print(lista[random.randrange(len(lista))])  # 67
 
 print(random.choice(lista))  # 68
+
+lista_kule = list(range(1, 50))  # od 1 do 49
+# print(lista_kule)
+
+wyn = random.choice(lista_kule)
+lista_kule.remove(wyn)
+print(wyn)
+
+print(random.choices(lista_kule, k=6))  # [2, 43, 43, 4, 13, 21], losuje z powtórzeniami
+print(random.sample(lista_kule, 6))  # [34, 14, 43, 36, 20, 39]
+print(random.sample(lista_kule, k=6))  # [37, 7, 30, 40, 34, 29], bez powtórzeń
