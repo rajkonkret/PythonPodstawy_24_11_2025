0ecf3e047d834d78624be07026da384293f100f1
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 15:03:33 +0100

    [3 30] update pliki_zad1.py.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -13,28 +13,29 @@
 # filehandler
 # context manager
 # with - kontekst manager
-with open("test.log", "w") as f:
+with open("test.log", "w", encoding='utf-8') as f:
     f.write("Powitanie\n")
 
 # FileExistsError: [Errno 17] File exists: 'test.log'
 # x - gdy plik istnieje bład
-# with open("test.log", "x") as f:
+# with open("test.log", "x", encoding='utf-8') as f:
 #     f.write("Powitanie\n")
 
-with open("test.log", "w") as f:
+with open("test.log", "w", encoding='utf-8') as f:
     f.write("Parametr1\n")
     f.write("Parametr2\n")
     f.write("Parametr3\n")
     f.write("Parametr4\n")
 
 # a - dodanie do istniejącego pliku
-with open("test.log", "a") as f:
+with open("test.log", "a", encoding='utf-8') as f:
     f.write("Dodane\n")
     f.write("Dodane2\n")
     f.write("Dodane3\n")
-    f.write("Dodane4\n")
+    f.write("Dśodane4\n")
 
-with open('test.log', "r") as file:
+# r - odczyt
+with open('test.log', "r", encoding='utf-8') as file:
     lines = file.read()
 
 print(lines)
