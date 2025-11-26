91f96b9b80213d0000cb972616d775b4de9c5677
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 14:53:17 +0100

    [3 29] update discount.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -15,3 +15,34 @@
 # with - kontekst manager
 with open("test.log", "w") as f:
     f.write("Powitanie\n")
+
+# FileExistsError: [Errno 17] File exists: 'test.log'
+# x - gdy plik istnieje bład
+# with open("test.log", "x") as f:
+#     f.write("Powitanie\n")
+
+with open("test.log", "w") as f:
+    f.write("Parametr1\n")
+    f.write("Parametr2\n")
+    f.write("Parametr3\n")
+    f.write("Parametr4\n")
+
+# a - dodanie do istniejącego pliku
+with open("test.log", "a") as f:
+    f.write("Dodane\n")
+    f.write("Dodane2\n")
+    f.write("Dodane3\n")
+    f.write("Dodane4\n")
+
+with open('test.log', "r") as file:
+    lines = file.read()
+
+print(lines)
+# Parametr1
+# Parametr2
+# Parametr3
+# Parametr4
+# Dodane
+# Dodane2
+# Dodane3
+# Dodane4
