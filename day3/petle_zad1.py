3c2150c1e7ca880d46196f76e880750b7aaa2a39
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 11:22:06 +0100

    [3 9] update petle_zad1.py

--- day3/petle_zad1.py
+++ day3/petle_zad1.py
@@ -49,3 +49,37 @@ print(lista3)  # [0, 2, 4, 6, 8]
 # list comprehensions
 lista3 = [j for j in range(10) if j % 2 == 0]
 print(lista3)  # [0, 2, 4, 6, 8]
+
+for c in lista3:  # podstawi kolejne elementy kolekcji
+    print(c)
+
+lista_nazwy = ["Ala", "Tomek", "Zenek", "Basia"]
+for p in lista_nazwy:
+    print(p)
+    # Ala
+    # Tomek
+    # Zenek
+    # Basia
+
+for c in lista3:
+    if c > 4:
+        print(c, "Wieksze od 4")
+    elif c == 4:
+        print(c, "Równe 4")
+    else:
+        print(c, "Mniejsze od 4")
+
+    print(c)  # dla kazdego elementu pętli
+
+print("Po zakońćzeniu pętli")
+# 0 Mniejsze od 4
+# 0
+# 2 Mniejsze od 4
+# 2
+# 4 Równe 4
+# 4
+# 6 Wieksze od 4
+# 6
+# 8 Wieksze od 4
+# 8
+# Po zakońćzeniu pętli
