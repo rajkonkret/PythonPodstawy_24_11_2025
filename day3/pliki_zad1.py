545724c82417c41a2193ae0c9c943d0319bd7555
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 14:41:19 +0100

    [3 28] update discount.py

---
+++ day3/pliki_zad1.py
@@ -0,0 +1,17 @@
+# działania z plikami
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
+# filehandler
+# context manager
+# with - kontekst manager
+with open("test.log", "w") as f:
+    f.write("Powitanie\n")
