2a3e6efc028819caee1b2d37af1a4c4553491c2b
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 11:14:58 +0100

    [3 78 update petle_zad1.py

--- day3/petle_zad1.py
+++ day3/petle_zad1.py
@@ -29,3 +29,23 @@ for _ in range(6):
     lista_wynik.append(wyn)
 
 print(lista_wynik)  # [23, 20, 17, 30, 31, 48]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+# parzyste do nowej listy
+lista3 = []
+for j in range(10):
+    if j % 2 == 0:
+        lista3.append(j)
+print(lista3)  # [0, 2, 4, 6, 8]
+
+# list comprehensions
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
