fab30282b84474e587b26896d835c1f843b7d59e
rajkonkre <rajkonkret660@gmail.com>
2025-11-26 15:31:27 +0100

    [3 32] update json_zad1.py

---
+++ day3/json_zad1.py
@@ -0,0 +1,15 @@
+# {"name":"John", "age":30, "car":null}
+#  {
+#         "description": "Zmienna to miejsce w pami\u0119ci komputera, gdzie przechowywana jest warto\u015b\u0107. W Pythonie zmienna przypisywana jest do nazwy za pomoc\u0105 operatora `=`.",
+#         "example": "nazwa = \"Python\"\nwiek = 30\nczy_aktywny = True",
+#         "id": 1,
+#         "level": "podstawowy",
+#         "term": "Co to jest zmienna w Pythonie?"
+#     },
+# json - dane typu klucz-wartość
+# popularny typ danych wymiany miedzy komputerami
+# odpowiednikiem jsona w pythonie jest -> słownik
+
+import json
+
+person_dict = {'name': 'Radek', 'age': 40, 'czy_pali': None}
