7b0c106c3c428fd63d3aff1e1aec4a1d94f5b7e3
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 12:28:35 +0100

    [4 22] update fun3.py

---
+++ day4/fun5_kantor.py
@@ -0,0 +1,4 @@
+# stworzyc funkcję kantor
+# ma miec dwie wewnętrzne funkcje: usd, eur
+# w zależności od parametrów ma zwrócić jedną lub drugę funkcja (if)
+# przekazenie argumentu (kwota) do funkcji usd i eur
