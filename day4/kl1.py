0020cac7dadf5abcb7418ba5c63828d01ac24d5e
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 15:28:12 +0100

    [4 38] update kl1

---
+++ day4/kl1.py
@@ -0,0 +1,22 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcje w klasie
+# obiekt - instancja klasy
+# klasa musi zostac zadeklarowana
+# tworzenie obiektu uruchamia funkcję inicjalizującą (konstruktor) __init__
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase, UpperCamelCase
+class Human:
+    """
+    Klasa Human opisujaca człowieka w pythonie
+    """
+
+
+# print(print.__doc__)
+print(Human.__doc__)  # Klasa Human opisujaca człowieka w pythonie
+# pydoc - dokumentacja
+#  cd day4 - wejscie do katalogu
+#  pydoc -b - serwer dokumentacji
+# cd .. - wyjscie katalog wyżej
+#  pydoc -w kl1 - generuje html z dokumentacją
