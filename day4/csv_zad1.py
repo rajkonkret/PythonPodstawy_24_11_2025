5b3d874af60986a71a5bfbe210c0e6474e5ca3e1
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 09:32:28 +0100

    [4 1] add csv_zad1.py

---
+++ day4/csv_zad1.py
@@ -0,0 +1,17 @@
+# csv - dane oddzielone znakiem podziału ,;tab
+# Kowalski,Jan,Kłodzko
+# Nowak,Zenon,Szczecin
+# Brzęczyszczykiewicz,Grzegorz,Chrząszczyżewoszyce
+
+import csv
+
+row = ['radek', 'coe', "3", 0]
+fields = ['name', 'branch', 'year', 'cgpa']
+
+filename = "records.csv"
+#  newline="" - obejscie problemu pustych linii
+with open(filename, "w", newline="") as csv_f:
+    csvwriter = csv.writer(csv_f)
+    csvwriter.writerow(fields)
+    csvwriter.writerow(row)
+# ctrl shift - strzalka do gory
