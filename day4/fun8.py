88de2f91fbb4d375214914da367c62f613dbfd59
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 14:02:12 +0100

    [4 28] update fun8.py

---
+++ day4/fun8.py
@@ -0,0 +1,16 @@
+def all_params(a, b, /, c=42, d=256):
+    print(f"{a=}, {b=}")
+    print(f"{c=}, {d=}")
+
+
+all_params(1, 2)
+# a=1, b=2
+# c=42, d=256
+all_params(1, 2, 3, 4)
+# a=1, b=2
+# c=3, d=4
+# TypeError: all_params() got some positional-only arguments passed as keyword arguments: 'a, b'
+# all_params(a=1, b=2, c=3, d=4)
+# / - wymusza koniecznośc podawania argumetów z lewej strony po pozycji
+all_params(1, 2, c=3, d=4)
+
