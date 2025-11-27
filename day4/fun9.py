94e28785a5935394faf70160f7206b030a374722
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 14:23:25 +0100

    [4 32] update fun8.py

--- day4/fun9.py
+++ day4/fun9.py
@@ -14,3 +14,10 @@ print(odejmij2(10, 8))  # 2
 
 oblicz_vat = lambda kwota, vat=23: kwota * (100 + vat) / 100
 print(oblicz_vat(1000))
+
+wiek = lambda x: "dziecko" if x < 10 else ("nastolatek" if x < 18 else "dorosły")
+print(wiek(9))  # dziecko
+print(wiek(10))  # nastolatek
+print(wiek(17))  # nastolatek
+print(wiek(18))  # dorosły
+print(wiek(29))  # dorosły
