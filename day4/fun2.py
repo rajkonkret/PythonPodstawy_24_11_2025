b7c409b01490507e11ca546d317066004fc15ab2
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 11:47:24 +0100

    [4 17] update fun1.py

---
+++ day4/fun2.py
@@ -0,0 +1,16 @@
+# funkcja zwracające wynik
+# kończy się return (zwróc)
+
+def dodaj(a, b):
+    return a + b
+
+
+def odejmij(a=0, b=0, c=0):
+    return a - b - c
+
+
+print(dodaj(6, 8))  # 14
+wynik = dodaj(6, 90)
+print("Wynik:", wynik)  # Wynik: 96
+
+
