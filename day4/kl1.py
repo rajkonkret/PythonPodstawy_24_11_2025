177a7ed148a0f10dbd3a0c54a1f23ca37436ac9c
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 15:32:48 +0100

    [4 39] update kl1

--- day4/kl1.py
+++ day4/kl1.py
@@ -11,12 +11,32 @@ class Human:
     """
     Klasa Human opisujaca człowieka w pythonie
     """
+    imie = ""
+    wiek = None
+    plec = "k"
 
 
 # print(print.__doc__)
-print(Human.__doc__)  # Klasa Human opisujaca człowieka w pythonie
+# print(Human.__doc__)  # Klasa Human opisujaca człowieka w pythonie
 # pydoc - dokumentacja
 #  cd day4 - wejscie do katalogu
 #  pydoc -b - serwer dokumentacji
 # cd .. - wyjscie katalog wyżej
 #  pydoc -w kl1 - generuje html z dokumentacją
+
+cz1 = Human()  # tworzenie obiektu
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+
+cz1.wiek = 44
+cz1.imie = "Radek"
+cz1.plec = "m"
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# Radek
+# 44
+# m
+
+cz2
