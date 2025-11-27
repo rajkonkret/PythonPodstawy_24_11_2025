69d5fb57c05ad464e65435d184f5c82439fd512f
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 14:06:51 +0100

    [4 29] update fun8.py

--- day4/fun8.py
+++ day4/fun8.py
@@ -14,3 +14,20 @@ all_params(1, 2, 3, 4)
 # / - wymusza koniecznośc podawania argumetów z lewej strony po pozycji
 all_params(1, 2, c=3, d=4)
 
+
+def all_params2(name, b, /, c=42, *args, **kwargs):
+    print(f"{name=}")
+    print(f"{b=}")
+    print(f"{c=}")
+    print(f"{args=}")
+    print(f"{kwargs=}")
+
+
+all_params2(1, 2, name="Radek")
+# name=1
+# b=2
+# c=42
+# args=()
+# kwargs={'name': 'Radek'}
+
+
