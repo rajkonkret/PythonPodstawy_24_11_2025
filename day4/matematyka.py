a413f7af023041e36f4f9f69d04cf9ed0359ede0
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 11:02:04 +0100

    [4 12] update csv_zad3.py

--- day4/matematyka.py
+++ day4/matematyka.py
@@ -11,3 +11,16 @@ print(angle_radians)  # 0.5235987755982988
 sin_value = math.sin(angle_radians)
 print(f"sin({angle_degree}) = {sin_value}")
 # sin(30) = 0.49999999999999994
+
+angles = [0, 30, 45, 60, 90]
+
+sin_values = [math.sin(math.radians(a)) for a in angles]
+
+for a, s in zip(angles, sin_values):
+    print(f"sin({a}) = {s:.2f}")
+# sin(30) = 0.49999999999999994
+# sin(0) = 0.00
+# sin(30) = 0.50
+# sin(45) = 0.71
+# sin(60) = 0.87
+# sin(90) = 1.00
