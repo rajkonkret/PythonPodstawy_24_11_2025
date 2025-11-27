f7db499c7d7a41d9b92deb272f136c776f95450c
rajkonkre <rajkonkret660@gmail.com>
2025-11-27 10:03:48 +0100

    [4 6] update csv_zad2.py

--- day4/csv_zad2.py
+++ day4/csv_zad2.py
@@ -1,12 +1,16 @@
 import csv
 
-filename = "records.csv"
+# filename = "records.csv"
+filename = "records_discount.csv"
 
 fields = []
 rows = []
 
 with open(filename, "r") as csv_f:
-    csvreader = csv.reader(csv_f)
+    dialect = csv.Sniffer().sniff(csv_f.read(1024))
+    print(dialect.delimiter)  # ;
+
+    csvreader = csv.reader(csv_f, delimiter=";")
     print(csvreader)
     # <_csv.reader object at 0x0000016B018041C0> - iterator
 
@@ -19,3 +23,6 @@ print('Fields:', fields)
 print("Rows:", rows)
 # Fields: ['name', 'branch', 'year', 'cgpa']
 # Rows: [['radek', 'coe', '3', '0']]
+
+# Fields: ['sku;exp_date;price']
+# Rows: [['1;today;200'], ['2;today;100'], ['3;tomorrow;50'], ['4;today;49.99'], ['5;today;100']]
