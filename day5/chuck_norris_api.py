8fbc7d918ea6ca98416c558fe883ef3b3983dd88
rajkonkre <rajkonkret660@gmail.com>
2025-11-28 11:56:32 +0100

    [5 5] update api chuck norris

--- day5/chuck_norris_api.py
+++ day5/chuck_norris_api.py
@@ -5,5 +5,14 @@
 # i otrzymywania odpowiedzi (responses) zazwyczaj w formacie JSON lub XML,
 # co pozwala na dostęp do danych i zarządzanie nimi
 # za pomocą standardowych metod HTTP (Create, Read, Update, Delete)
+import requests
 
+#  pip install requests
 url = "https://api.chucknorris.io/jokes/random"
+
+response = requests.get(url)
+print(response)  # <Response [200]>
+# 200 ok
+# 3xx - warningi, przekierowanie
+# 4xx, 404 - brak zasobu, 400 Bad Req
+# 5xx - 500 Internal Server Error
