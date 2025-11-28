1815ad189db01dcc363da41b00f60b6d2a3b3b3e
rajkonkre <rajkonkret660@gmail.com>
2025-11-28 13:00:28 +0100

    [5 17] baza2.py update

--- day5/baza2.py
+++ day5/baza2.py
@@ -5,31 +5,52 @@ try:
     c = conn.cursor()
     print("Baza danych została podłaczona")
 
-    query = """CREATE TABLE IF NOT EXISTS developers (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    salary REAL NOT NULL);"""
-
+    query = """CREATE TABLE IF NOT EXISTS developers
+               (
+                   id
+                   INTEGER
+                   PRIMARY
+                   KEY,
+                   name
+                   TEXT
+                   NOT
+                   NULL,
+                   email
+                   TEXT
+                   NOT
+                   NULL
+                   UNIQUE,
+                   salary
+                   REAL
+                   NOT
+                   NULL
+               );"""
 
     c.execute(query)
     conn.commit()
 
     insert = """INSERT INTO developers (id, name, email, salary)
-             VALUES (1,'Radek','raj@raj.pl', 10000);"""
+                VALUES (1, 'Radek', 'raj@raj.pl', 10000);"""
 
     # c.execute(insert)
     # conn.commit()
 
     select = "SELECT * FROM developers;"
     for row in c.execute(select):
-        print(row) # (1, 'Radek', 'raj@raj.pl', 10000.0)
+        print(row)  # (1, 'Radek', 'raj@raj.pl', 10000.0)
 
-    update = """UPDATE developers SET salary=20000 WHERE id=1;"""
+    update = """UPDATE developers
+                SET salary=20000
+                WHERE id = 1;"""
     c.execute(update)
     conn.commit()
 
+    delete = """DELETE
+                FROM developers
+                WHERE id = 1;"""
 
+    c.execute(delete)
+    conn.commit()
 except sqlite3.Error as e:
     print("Bład podłaczenia z bazą:", e)
 finally:
