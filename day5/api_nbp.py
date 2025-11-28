4bfa24672148c47bd4edce4db37aee8aba671044
rajkonkre <rajkonkret660@gmail.com>
2025-11-28 12:12:32 +0100

    [5 8] update api nbp

--- day5/api_nbp.py
+++ day5/api_nbp.py
@@ -1,1 +1,13 @@
+import requests
+
 url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
+
+response = requests.get(url)
+print(response)
+data = response.json()
+print(data)
+# {'table': 'A',
+# 'currency': 'euro',
+# 'code': 'EUR',
+# 'rates': [{'no': '231/A/NBP/2025', 'effectiveDate': '2025-11-28', 'mid': 4.2369}]}
+
