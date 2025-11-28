902da58c0c774d0d6ac925440302204a0734fda2
rajkonkre <rajkonkret660@gmail.com>
2025-11-28 11:50:34 +0100

    [5 4] api chuck norris

---
+++ day5/chuck_norris_api.py
@@ -0,0 +1,9 @@
+# REST API (Representational State Transfer) to styl
+# architektury oprogramowania umożliwiający komunikację między różnymi systemami przez internet,
+# np. między aplikacją mobilną a serwerem.
+# Działa na zasadzie wysyłania żądań (requests)
+# i otrzymywania odpowiedzi (responses) zazwyczaj w formacie JSON lub XML,
+# co pozwala na dostęp do danych i zarządzanie nimi
+# za pomocą standardowych metod HTTP (Create, Read, Update, Delete)
+
+url = "https://api.chucknorris.io/jokes/random"
