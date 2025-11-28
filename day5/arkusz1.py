5f88e1afb4ea009377faaf73db73a40acd73b338
rajkonkre <rajkonkret660@gmail.com>
2025-11-28 14:20:27 +0100

    [5 22] update arkusz1

--- day5/arkusz1.py
+++ day5/arkusz1.py
@@ -16,3 +16,15 @@ for i in worksheet:
 # (<Cell 'Arkusz1'.A3>, <Cell 'Arkusz1'.B3>, <Cell 'Arkusz1'.C3>)
 # (<Cell 'Arkusz1'.A4>, <Cell 'Arkusz1'.B4>, <Cell 'Arkusz1'.C4>)
 # (<Cell 'Arkusz1'.A5>, <Cell 'Arkusz1'.B5>, <Cell 'Arkusz1'.C5>)
+
+for i in range(0, worksheet.max_row):
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        lista.append(col[i].value)
+
+print(lista)
+# ['Sales Date', 'Sales Person', 'Amount',
+#  datetime.datetime(2018, 5, 12, 0, 0), 'Sila Ahmed', 60000,
+#  datetime.datetime(2019, 12, 6, 0, 0), 'Mir Hossain', 50000,
+#  datetime.datetime(2020, 8, 9, 0, 0), 'Sarmin Jahan', 45000,
+#  datetime.datetime(2021, 4, 7, 0, 0), 'Mahmudul Hasan', 30000]
+
